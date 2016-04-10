49009e984d69bceafde989de1d84721ee1d1828d
wangmengcn <eclipse_sv@163.com>
2016-04-10 13:44:00 +0800

    0410

--- tag_collection.py
+++ tag_collection.py
@@ -1,36 +1,32 @@
-#-*- coding: UTF-8 -*- 
+#-*- coding: UTF-8 -*-
 # 通过传入的类别，获取不同类型的标签
 
 import re
 
+
 def get_tags(category):
-	filename = ''
+    filename = ''
 
-	if category == "movie":
-		filename = './tags/movies.html'
-	elif category == "book":
-		filename = './tags/books.html'
-	elif category == "music":
-		filename = './tags/music.html'
-	else:
-		print "还未添加此类别"
+    if category == "movie":
+        filename = './tags/movies.html'
+    elif category == "book":
+        filename = './tags/books.html'
+    elif category == "music":
+        filename = './tags/music.html'
+    else:
+        print "还未添加此类别"
 
-	return match_tag(filename)
+    return match_tag(filename)
 
 
 # 通过文件名 获取标签项
 def match_tag(filename):
-	filetags=[]
-	file = open(filename)
-	for line in file.xreadlines():
-		# 匹配对应文件的标签
-	    book_tag = re.findall(r'class="tag">(.*?)</a>',line,re.S)
-	    if book_tag:
-	    	for item in book_tag:
-	    		filetags.append(item)
-	return filetags	  
-
-
-
-
-	  
+    filetags = []
+    file = open(filename)
+    for line in file.xreadlines():
+        # 匹配对应文件的标签
+        book_tag = re.findall(r'class="tag">(.*?)</a>', line, re.S)
+        if book_tag:
+            for item in book_tag:
+                filetags.append(item)
+    return filetags
